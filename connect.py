b8b58b4ef5490d64d40cb1c2d6e4dbafe57ff215
Paweł Szumański <pawel.szumanski@tuta.io>
2024-02-24 16:37:40 +0100

    Add try-except for connection error

--- connect.py
+++ connect.py
@@ -1,4 +1,7 @@
+import sys
+
 from mongoengine import connect
+from pymongo.errors import ConfigurationError
 import configparser
 
 config = configparser.ConfigParser()
@@ -9,9 +12,13 @@ mongodb_pass = config.get("DB", "pass")
 db_name = config.get("DB", "db_name")
 domain = config.get("DB", "domain")
 
-# connect to cluster on AtlasDB with connection string
-
-connect(
-    host=f"""mongodb+srv://{mongo_user}:{mongodb_pass}@{domain}/{db_name}?retryWrites=true&w=majority""",
-    ssl=True,
-)
+try:
+    # connect to cluster on AtlasDB with connection string
+    connect(
+        host=f"""mongodb+srv://{mongo_user}:{mongodb_pass}@{domain}/{db_name}?retryWrites=true&w=majority""",
+        ssl=True,
+    )
+except ConfigurationError as e:
+    print("Problem with database connection, check your connection and credentials settings.")
+    print(f"Error: {e}")
+    sys.exit(1)
